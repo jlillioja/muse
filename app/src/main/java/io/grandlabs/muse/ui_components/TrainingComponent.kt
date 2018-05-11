28df3e24361d005f1833f0885f848ee400dc01bc
jlillioja <jlillioja@gmail.com>
2018-05-10 19:32:02 -0500

    Fuckin finished goddamn training (for SML)

--- app/src/main/java/io/grandlabs/muse/ui_components/TrainingComponent.kt
+++ app/src/main/java/io/grandlabs/muse/ui_components/TrainingComponent.kt
@@ -1,6 +1,7 @@
 package io.grandlabs.muse.ui_components
 
-import android.animation.ValueAnimator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.Drawable
@@ -9,7 +10,6 @@ import android.support.constraint.ConstraintSet
 import android.support.constraint.ConstraintSet.MATCH_CONSTRAINT_WRAP
 import android.support.constraint.ConstraintSet.PARENT_ID
 import android.support.v4.content.res.ResourcesCompat
-import android.transition.TransitionManager
 import android.view.Gravity
 import android.view.View
 import android.view.View.*
@@ -22,15 +22,11 @@ import io.grandlabs.muse.NavigationAction
 import io.grandlabs.muse.NavigationController
 import io.grandlabs.muse.R
 import org.jetbrains.anko.*
-import org.jetbrains.anko.constraint.layout.ConstraintSetBuilder
 import org.jetbrains.anko.constraint.layout.ConstraintSetBuilder.Side.*
 import org.jetbrains.anko.constraint.layout.applyConstraintSet
 import org.jetbrains.anko.constraint.layout.constraintLayout
 import org.jetbrains.anko.sdk25.coroutines.onClick
 import javax.inject.Inject
-import android.view.animation.BounceInterpolator
-import android.transition.ChangeBounds
-import org.jetbrains.anko.constraint.layout.constraintSet
 
 
 class TrainingComponent
@@ -39,7 +35,7 @@ class TrainingComponent
         private val ctx: Context
 ) : AnkoComponent<MuseFragment> {
 
-    private val numberOfTrainingStates = 16
+    private val numberOfTrainingStates = 20
 
     private lateinit var constraintLayout: ConstraintLayout
     private lateinit var backButton: Button
@@ -50,11 +46,19 @@ class TrainingComponent
 
     private lateinit var miaLeftAndDown: ImageView
     private lateinit var mealIcon: ImageView
+    private lateinit var homeIcon: ImageView
 
     private lateinit var textBottomSpace: Space
 
     private lateinit var progressCircle: ImageView
+    private lateinit var progressMeter: ImageView
+    private lateinit var progressText: TextView
+    private lateinit var starStart: Space
     private lateinit var star1: ImageView
+    private lateinit var star2: ImageView
+    private lateinit var star3: ImageView
+    private lateinit var star4: ImageView
+    private lateinit var star5: ImageView
 
 
     override fun createView(ui: AnkoContext<MuseFragment>): View = with(ui) {
@@ -85,7 +89,7 @@ class TrainingComponent
                         moveToTrainingState(trainingState - 1)
                     }
                 }
-            }
+            }.lparams(dip(120), wrapContent)
 
             nextButton = button {
                 id = generateViewId()
@@ -96,7 +100,7 @@ class TrainingComponent
                 onClick {
                     moveToTrainingState(trainingState + 1)
                 }
-            }
+            }.lparams(dip(120), wrapContent)
 
             saveButton = button {
                 id = generateViewId()
@@ -124,12 +128,47 @@ class TrainingComponent
                 }
             }
 
+            homeIcon = imageView(R.drawable.icon_home) {
+                id = generateViewId()
+                visibility = GONE
+
+                onClick {
+                    moveToTrainingState(trainingState + 1)
+                }
+            }
+
             progressCircle = imageView(R.drawable.progress_foreground) {
                 id = generateViewId()
                 visibility = GONE
+                backgroundDrawable = resources.getDrawable(R.drawable.progress_background)
             }.lparams(dip(254), dip(254))
 
+            progressMeter = imageView(R.drawable.progress_1) {
+                id = generateViewId()
+                visibility = GONE
+            }
+
+            starStart = space {
+                id = generateViewId()
+            }
+
             star1 = imageView(R.drawable.star) {
+                id = generateViewId()
+                visibility = GONE
+            }.lparams(dip(58), dip(63))
+            star2 = imageView(R.drawable.star) {
+                id = generateViewId()
+                visibility = GONE
+            }.lparams(dip(58), dip(63))
+            star3 = imageView(R.drawable.star) {
+                id = generateViewId()
+                visibility = GONE
+            }.lparams(dip(58), dip(63))
+            star4 = imageView(R.drawable.star) {
+                id = generateViewId()
+                visibility = GONE
+            }.lparams(dip(58), dip(63))
+            star5 = imageView(R.drawable.star) {
                 id = generateViewId()
                 visibility = GONE
             }.lparams(dip(58), dip(63))
@@ -147,7 +186,7 @@ class TrainingComponent
                 }
 
                 textBottomSpace {
-                    connect(BOTTOM to BOTTOM of titleText margin ctx.dip(50))
+                    connect(BOTTOM to BOTTOM of titleText margin ctx.dip(25))
                 }
 
                 backButton {
@@ -186,23 +225,25 @@ class TrainingComponent
                             BOTTOM to BOTTOM of PARENT_ID margin dip(51))
                 }
 
+                homeIcon {
+                    connect(LEFT to LEFT of PARENT_ID margin dip(34),
+                            BOTTOM to BOTTOM of PARENT_ID margin dip(51))
+                }
+
                 progressCircle {
                     connect(TOP to TOP of PARENT_ID margin dip(106),
                             LEFT to LEFT of PARENT_ID,
                             RIGHT to RIGHT of PARENT_ID)
                 }
 
-                star1 {
-                    connect(
-                            BOTTOM to TOP of PARENT_ID,
-                            LEFT to LEFT of PARENT_ID,
-                            RIGHT to RIGHT of PARENT_ID
-                    )
+                progressMeter {
+                    connect(TOP to TOP of progressCircle,
+                            LEFT to LEFT of progressCircle,
+                            RIGHT to RIGHT of progressCircle,
+                            BOTTOM to BOTTOM of progressCircle)
                 }
 
             }
-
-
         }
         moveToTrainingState(1)
         return constraintLayout
@@ -219,8 +260,16 @@ class TrainingComponent
             nextButton.visibility = VISIBLE
             miaLeftAndDown.visibility = GONE
             mealIcon.visibility = GONE
+            homeIcon.visibility = GONE
             progressCircle.visibility = GONE
+            progressMeter.visibility = GONE
+            star1.visibility = GONE
+            star2.visibility = GONE
+            star3.visibility = GONE
+            star4.visibility = GONE
+            star5.visibility = GONE
             defaultConstraints.applyTo(constraintLayout)
+
 
             if (trainingState == 8) {
                 nextButton.visibility = GONE
@@ -235,6 +284,7 @@ class TrainingComponent
                         connect(
                                 TOP to BOTTOM of textBottomSpace
                         )
+                        verticalBias = 0f
                     }
                 }
             }
@@ -244,46 +294,183 @@ class TrainingComponent
                         connect(TOP to BOTTOM of textBottomSpace,
                                 LEFT to LEFT of PARENT_ID,
                                 RIGHT to LEFT of nextButton)
-                        verticalBias = 0.1f
+                        verticalBias = 0f
                     }
 
                     nextButton {
                         connect(TOP to BOTTOM of textBottomSpace,
                                 LEFT to RIGHT of backButton,
                                 RIGHT to RIGHT of PARENT_ID)
-                        verticalBias = 0.1f
+                        verticalBias = 0f
                     }
                 }
 
                 if (trainingState == 14) {
                     nextButton.visibility = GONE
                     saveButton.visibility = VISIBLE
-                } else {
-                    nextButton.visibility = VISIBLE
-                    saveButton.visibility = GONE
                 }
             }
             if (trainingState == 15) {
+                val starStartPosition = ctx.dip(-200).toFloat()
+                val starEndPosition = ctx.dip(210).toFloat()
+
                 progressCircle.visibility = VISIBLE
+                progressMeter.visibility = VISIBLE
+                progressMeter.alpha = 0f
+
                 star1.visibility = VISIBLE
+                star1.translationY = starStartPosition
+                star1.translationX = ctx.dip(176).toFloat()
+
+                star2.visibility = VISIBLE
+                star2.translationY = starStartPosition
+                star2.translationX = ctx.dip(200).toFloat()
+
+                star3.visibility = VISIBLE
+                star3.translationY = starStartPosition
+                star3.translationX = ctx.dip(150).toFloat()
+
+                star4.visibility = VISIBLE
+                star4.translationY = starStartPosition
+                star4.translationX = ctx.dip(210).toFloat()
+
+                star5.visibility = VISIBLE
+                star5.translationY = starStartPosition
+                star5.translationX = ctx.dip(100).toFloat()
 
                 titleText.visibility = GONE
                 backButton.visibility = GONE
                 nextButton.visibility = GONE
 
-                val constraints = constraintLayout.constraintSet {
-                    connect(TOP of star1 to TOP of PARENT_ID margin ctx.dip(210))
+                val animationStartTime: Long = 300
+                val animationOffset: Long = 100
+                val slideDuration: Long = 750
+                val fadeDuration: Long = 750
+
+                val star1SlideInStartTime: Long = animationStartTime
+                val star2SlideInStartTime: Long = star1SlideInStartTime + animationOffset
+                val star3SlideInStartTime: Long = star2SlideInStartTime + animationOffset
+                val star4SlideInStartTime: Long = star3SlideInStartTime + animationOffset
+                val star5SlideInStartTime: Long = star4SlideInStartTime + animationOffset
+
+                val star1slideAnimation = ObjectAnimator
+                        .ofFloat(star1, View.TRANSLATION_Y, starEndPosition)
+                        .setDuration(slideDuration)
+                        .apply { startDelay = star1SlideInStartTime }
+
+                val star1fadeAnimation = ObjectAnimator
+                        .ofFloat(star1, View.ALPHA, 0f)
+                        .setDuration(fadeDuration)
+
+                val star1Animations = AnimatorSet()
+                star1Animations.playSequentially(star1slideAnimation, star1fadeAnimation)
+                star1Animations.start()
+
+                val star2slideAnimation = ObjectAnimator
+                        .ofFloat(star2, View.TRANSLATION_Y, starEndPosition)
+                        .setDuration(slideDuration)
+                        .apply { startDelay = star2SlideInStartTime }
+
+                val star2fadeAnimation = ObjectAnimator
+                        .ofFloat(star2, View.ALPHA, 0f)
+                        .setDuration(fadeDuration)
+
+                val star2Animations = AnimatorSet()
+                star2Animations.playSequentially(star2slideAnimation, star2fadeAnimation)
+                star2Animations.start()
+
+                val star3slideAnimation = ObjectAnimator
+                        .ofFloat(star3, View.TRANSLATION_Y, starEndPosition)
+                        .setDuration(slideDuration)
+                        .apply { startDelay = star3SlideInStartTime }
+
+                val star3fadeAnimation = ObjectAnimator
+                        .ofFloat(star3, View.ALPHA, 0f)
+                        .setDuration(fadeDuration)
+
+                val star3Animations = AnimatorSet()
+                star3Animations.playSequentially(star3slideAnimation, star3fadeAnimation)
+                star3Animations.start()
+
+                val star4slideAnimation = ObjectAnimator
+                        .ofFloat(star4, View.TRANSLATION_Y, starEndPosition)
+                        .setDuration(slideDuration)
+                        .apply { startDelay = star4SlideInStartTime }
+
+                val star4fadeAnimation = ObjectAnimator
+                        .ofFloat(star4, View.ALPHA, 0f)
+                        .setDuration(fadeDuration)
+
+                val star4Animations = AnimatorSet()
+                star4Animations.playSequentially(star4slideAnimation, star4fadeAnimation)
+                star4Animations.start()
+
+                val star5slideAnimation = ObjectAnimator
+                        .ofFloat(star5, View.TRANSLATION_Y, starEndPosition)
+                        .setDuration(slideDuration)
+                        .apply { startDelay = star5SlideInStartTime }
+
+                val star5fadeAnimation = ObjectAnimator
+                        .ofFloat(star5, View.ALPHA, 0f)
+                        .setDuration(fadeDuration)
+
+                val star5Animations = AnimatorSet()
+                star5Animations.playSequentially(star5slideAnimation, star5fadeAnimation)
+                star5Animations.start()
+
+                ObjectAnimator
+                        .ofFloat(progressMeter, View.ALPHA, 1f)
+                        .apply { startDelay = 1000 }
+                        .start()
+
+                constraintLayout.postDelayed({ moveToTrainingState(16) }, 2500)
+            }
+            if (trainingState == 16) {
+                constraintLayout.applyConstraintSet {
+                    titleText {
+                        connect(TOP to TOP of PARENT_ID margin ctx.dip(363))
+                    }
+
+                    backButton {
+                        connect(TOP to BOTTOM of textBottomSpace,
+                                LEFT to LEFT of PARENT_ID,
+                                RIGHT to LEFT of nextButton)
+                        verticalBias = 0f
+                    }
+
+                    nextButton {
+                        connect(TOP to BOTTOM of textBottomSpace,
+                                LEFT to RIGHT of backButton,
+                                RIGHT to RIGHT of PARENT_ID)
+                        verticalBias = 0f
+                    }
                 }
+            }
+            if (trainingState == 18) {
+                homeIcon.visibility = VISIBLE
+                nextButton.visibility = GONE
 
-                constraintLayout.postDelayed({
-                    val transition = ChangeBounds()
-//                    transition.interpolator = BounceInterpolator()
-                    transition.duration = 1000
-                    TransitionManager.beginDelayedTransition(constraintLayout, transition)
-                    constraints.applyTo(constraintLayout)
-                }, 10)
+                constraintLayout.applyConstraintSet {
+                    titleText {
+                        connect(TOP to TOP of PARENT_ID margin ctx.dip(374))
+                    }
 
-//                moveToTrainingState(16)
+                    backButton {
+                        connect(TOP to BOTTOM of textBottomSpace,
+                                LEFT to LEFT of titleText margin ctx.dip(16))
+                        verticalBias = 0f
+                        horizontalBias = 0f
+                    }
+
+
+                }
+            }
+            if ((trainingState == 19) or (trainingState == 20)) {
+                constraintLayout.applyConstraintSet {
+                    titleText {
+                        connect(TOP to TOP of PARENT_ID margin ctx.dip(369))
+                    }
+                }
             }
         } else navigationController.navigateTo(NavigationAction.HOME)
     }
@@ -305,6 +492,10 @@ class TrainingComponent
         14 -> "When you are done entering the meal, save it.\n"
         15 -> ""
         16 -> "We’ll earn stars each time you log a meal, as I learn more about how your body reacts to food and insulin doses. When the bucket's full, we’re ready to fly! Then I’ll start suggesting insulin doses that are unique to you.\n"
+        17 -> "The meals you log will be listed here."
+        18 -> "Touch \"home\" to return to the nest.\n"
+        19 -> "As a lifelong learner, I’ll keep learning as long as you keep logging, so that I can keep recommending safe, personalized insulin doses."
+        20 -> "That’s it!\nJust a few stars to go and we can fly together.\nSee you at dinner time!"
         else -> "I've got problems!"
 
     }
@@ -327,6 +518,11 @@ class TrainingComponent
             13 -> ctx.resources.getDrawable(R.drawable.ob_110_a_bg)
             14 -> ctx.resources.getDrawable(R.drawable.ob_120_a_bg)
             15 -> ctx.resources.getDrawable(R.drawable.ob_prob_anim_bg)
+            16 -> ctx.resources.getDrawable(R.drawable.ob_150_bg)
+            17 -> ctx.resources.getDrawable(R.drawable.ob_160_a_bg)
+            18 -> ctx.resources.getDrawable(R.drawable.ob_170_bg)
+            19 -> ctx.resources.getDrawable(R.drawable.ob_180_bg)
+            20 -> ctx.resources.getDrawable(R.drawable.ob_190_bg)
             else -> null
         }
     }
